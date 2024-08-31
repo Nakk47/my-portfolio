a808ccb3c707fc026db8b99c27c5ccb049e93683
Nakk47 <pozz.vathhd@gmail.com>
2024-08-31 10:58:02 +0700

    coming soon

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-function scrollToBottom()
-{
-    window.scrollTo(0, 6040);
-}
 
-function scrollToTop()
+function scrollToHome()
 {
     window.scrollTo(0, 0);
 }
 
 function scrollToAbout()
 {
-    window.scrollTo(0, 900);
+    window.scrollTo(0, 850);
 }
 
 function scrollToSkill()
 {
-    window.scrollTo(0, 900);
+    window.scrollTo(0, 1750);
+}
+
+function scrollToEducation()
+{
+    window.scrollTo(0, 2650);
+}
+
+function scrollToExperience()
+{
+    window.scrollTo(0, 3550);
+}
+
+function scrollToContact()
+{
+    window.scrollTo(0, 4450);
 }
